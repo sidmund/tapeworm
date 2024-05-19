173f098ac52c7201e59062a09f7a281f8370b0ef
sidmund <sidrosterij@gmail.com>
2024-05-19 17:02:56 +0200

    refactor(download): optimizations

--- src/download.rs
+++ src/download.rs
@@ -1,29 +1,32 @@
 //! Download all inputs in the library.
 
-use crate::types;
-use crate::util;
-use crate::Config;
+use crate::{types, util, Config};
 use std::collections::HashSet;
 use std::fs;
 use std::io::{BufRead, BufReader, ErrorKind};
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 pub fn download<R: BufRead>(config: &Config, reader: R) -> types::UnitResult {
-    let use_yt_dlp_conf = if let Some(value) = yt_dlp_conf_exists(config, reader)? {
-        value
-    } else {
-        return Ok(()); // User wants to abort when config is not found
-    };
-
-    if fs::metadata(&config.input_path.clone().unwrap()).is_err() {
-        return Err(format!(
-            "Input file not found: {}",
-            config.input_path.clone().unwrap().to_str().unwrap()
-        )
-        .into());
+    let mut yt_dlp_conf_path = config.yt_dlp_conf_path.clone();
+    if fs::metadata(yt_dlp_conf_path.as_ref().unwrap()).is_err() {
+        println!(
+            "Warning: {} not found
+If you continue, yt-dlp will be invoked without any options, which will yield inconsistent results.",
+            yt_dlp_conf_path.unwrap().to_str().unwrap()
+        );
+        if !util::confirm("Do you want to continue regardless?", false, reader)? {
+            return Ok(()); // User wants to abort when config is not found
+        }
+        yt_dlp_conf_path = None;
     }
 
-    let inputs = fs::read_to_string(&config.input_path.clone().unwrap())?;
+    let input_path = config.input_path.clone().unwrap();
+    if fs::metadata(&input_path).is_err() {
+        return Err(format!("Input file not found: {}", input_path.to_str().unwrap()).into());
+    }
+
+    let inputs = fs::read_to_string(&input_path)?;
     if inputs.is_empty() {
         if config.verbose {
             println!("Nothing to download. Library is empty.");
@@ -32,28 +35,24 @@ pub fn download<R: BufRead>(config: &Config, reader: R) -> types::UnitResult {
     }
 
     let inputs: HashSet<String> = inputs.lines().map(|s| s.to_string()).collect();
-
     if config.verbose {
         println!("Downloading {} URLs:", inputs.len());
         inputs.iter().for_each(|s| println!("  {}", s));
     }
-
-    yt_dlp(&config, use_yt_dlp_conf, inputs)?;
+    yt_dlp(yt_dlp_conf_path, inputs)?;
 
     if config.clear_input {
-        fs::write(&config.input_path.clone().unwrap(), "")?;
+        fs::write(&input_path, "")?;
     }
 
     Ok(())
 }
 
 /// Download URLs with yt-dlp
-fn yt_dlp(config: &Config, use_conf: bool, urls: HashSet<String>) -> types::UnitResult {
+fn yt_dlp(conf_path: Option<PathBuf>, urls: HashSet<String>) -> types::UnitResult {
     let mut command = Command::new("yt-dlp");
-    if use_conf {
-        command
-            .arg("--config-location")
-            .arg(&config.yt_dlp_conf_path.clone().unwrap());
+    if let Some(conf_path) = conf_path {
+        command.arg("--config-location").arg(conf_path);
     }
     urls.iter().for_each(|url| {
         command.arg(url);
@@ -70,26 +69,4 @@ fn yt_dlp(config: &Config, use_conf: bool, urls: HashSet<String>) -> types::Unit
         .for_each(|line| println!("{}", line));
 
     Ok(())
-}
-
-/// Returns:
-/// - Some(true) if yt-dlp.conf exists, it will be used
-/// - Some(false) if the user wants to continue without yt-dlp.conf
-/// - None if the user wants to abort
-fn yt_dlp_conf_exists<R: BufRead>(config: &Config, reader: R) -> types::OptionBoolResult {
-    if fs::metadata(&config.yt_dlp_conf_path.clone().unwrap()).is_ok() {
-        return Ok(Some(true));
-    }
-
-    println!(
-            "Warning: {} not found
-If you continue, yt-dlp will be invoked without any options, which will yield inconsistent results.",
-            config.yt_dlp_conf_path.clone().unwrap().to_str().unwrap()
-        );
-
-    if util::confirm("Do you want to continue regardless?", false, reader)? {
-        Ok(Some(false))
-    } else {
-        Ok(None)
-    }
 }
--- src/types.rs
+++ src/types.rs
@@ -6,7 +6,6 @@ use std::path::PathBuf;
 pub type BoolResult = Result<bool, Box<dyn Error>>;
 pub type ConfigResult = Result<Config, Box<dyn Error>>;
 pub type HashSetResult = Result<HashSet<String>, Box<dyn Error>>;
-pub type OptionBoolResult = Result<Option<bool>, Box<dyn Error>>;
 pub type OptionVecString = Option<Vec<String>>;
 pub type PathBufResult = Result<PathBuf, Box<dyn Error>>;
 pub type StringResult = Result<String, Box<dyn Error>>;
