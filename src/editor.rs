825e3c8330b1479f102d06a0d5af4abbc0718b1f
sidmund <sidrosterij@gmail.com>
2024-05-28 10:53:33 +0200

    style: improve tag editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,20 +1,21 @@
 //! Tag editor functions.
 
 use crate::{types, util};
-use std::{collections::HashMap, io::BufRead};
+use std::collections::HashMap;
+use std::io::{BufRead, Write};
 
 /// # Returns
 /// `HashMap<String, Option<String>>`:
 /// - The `String` key is the tag name
 /// - The `Option` is the value: `None` to clear it, `Some(String)` to set/update it
 pub fn edit<R: BufRead>(mut reader: R) -> types::HashMapResult {
-    println!("tte> ===== Tapeworm Tag Editor =====");
+    println!("\n===== Tapeworm Tag Editor =====");
     tag_editor_help();
 
     let mut edits = HashMap::new();
-
     loop {
-        println!("tte> ?: ");
+        print!("?> ");
+        std::io::stdout().flush().unwrap();
         let cmd = util::input(&mut reader, false)?;
         match cmd.as_str() {
             "quit" | "q" => break,
@@ -28,17 +29,14 @@ pub fn edit<R: BufRead>(mut reader: R) -> types::HashMapResult {
             }
         }
     }
-
     Ok(edits)
 }
 
 fn parse(cmd: String) -> Option<(String, Option<String>)> {
-    let split = cmd.split_once(' ');
-    let (tag_name, tag_value) = if split.is_none() {
-        (cmd.to_uppercase(), None)
-    } else {
-        let (k, v) = split.unwrap();
+    let (tag_name, tag_value) = if let Some((k, v)) = cmd.split_once(' ') {
         (k.to_uppercase(), Some(String::from(v)))
+    } else {
+        (cmd.to_uppercase(), None)
     };
 
     match tag_name.as_str() {
@@ -51,11 +49,11 @@ fn parse(cmd: String) -> Option<(String, Option<String>)> {
 
 fn tag_editor_help() {
     println!("\
-tte> Commands:
-tte>     quit, q         Go back to \"Proposed changes\" (asks to confirm your edits, if any)
-tte>     help, h         Show this help menu
-tte>     TAG             Clear TAG value
-tte>     TAG VALUE       Set TAG to VALUE (ARTIST may have multiple with ';'), e.g.: `ARTIST The Band;Singer`, `ARTIST Rapper`
-tte> Supported tags (lowercase also allowed):
-tte>    ARTIST, ALBUM, ALBUM_ARTIST, GENRE, TITLE, TRACK, YEAR");
+Commands:
+  quit, q         Go back to \"Proposed changes\" (asks to confirm your edits, if any)
+  help, h         Show this help menu
+  TAG             Clear TAG value
+  TAG VALUE       Set TAG to VALUE (ARTIST may have multiple with ';'), e.g.: `ARTIST The Band;Singer`, `ARTIST Rapper`
+Supported tags (lowercase also allowed):
+  ARTIST, ALBUM, ALBUM_ARTIST, GENRE, TITLE, TRACK, YEAR");
 }
